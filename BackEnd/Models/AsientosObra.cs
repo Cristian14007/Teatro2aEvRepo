994564a33766f29e4a1e504cb3a99eb83bada38b
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-27 19:57:46 +0100

    realtion asientos/obra

---
+++ BackEnd/Models/AsientosObra.cs
@@ -0,0 +1,32 @@
+namespace BackEnd.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+public class AsientosObra
+{
+    [Key]
+    public int Asiento_Obra_Id { get; set; }
+    [Required]
+    public bool Reservado { get; set; }
+
+    [ForeignKey ("Obra")]
+    public int ObraId { get; set;}
+    public Obra Obra { get; set; }
+    
+    [ForeignKey ("Asiento")]
+     public int AsientoId { get; set; }
+
+    public Asiento Asiento { get; set; }
+    
+    public AsientosObra() { }
+    public AsientosObra(int asientoId, bool reservado, int obraId)
+    {
+        Reservado = reservado;
+        AsientoId = asientoId;
+        ObraId = obraId;
+         
+    }
+
+    
+}
---
+++ BackEnd/Models/ObraUpdateDTO.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd.Models;
+
+public class ObraUpdateDTO
+{
+    [Required]
+    [StringLength(30, ErrorMessage = "El titulo de la obra debe tener menos de 30 caracteres")]
+    public string Titulo { get; set; }
+
+    [Required]
+    [StringLength(100, ErrorMessage = "La descripcion de la obra debe tener menos de 100 caracteres")]
+    public decimal Descripcion { get; set; }
+
+    [Required]
+    public decimal Genero { get; set; }
+
+    [Required]
+    public decimal Imagen { get; set; }
+}
+
